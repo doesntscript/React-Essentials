6a4acc02829205f1ddcf2adcb465c7422d407803
Guilherme <119014317+Guilhermezzq@users.noreply.github.com>
2024-09-19 14:41:01 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+
+
+import reactImg from './assets/..';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
   return (
     <>
     <header>
-        <img src="" alt="" />
+        <img src={reactImg} alt="" />
         <h1>React Essentials</h1>
-        <p>Fundamental React concepts you will need for almost any app you are
-        going to build!</p>
+        <p>
+          {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are
+        going to build!
+        </p>
       </header>
     </>
   )
