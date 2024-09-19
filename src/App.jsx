30dd048ba8b372a4e1446a6583cdffc39c06468d
Guilherme <119014317+Guilhermezzq@users.noreply.github.com>
2024-09-19 13:58:05 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+function Header() {
+  return (
+    <>
+    <header>
+        <img src="" alt="" />
+        <h1>React Essentials</h1>
+        <p>Fundamental React concepts you will need for almost any app you are
+        going to build!</p>
+      </header>
+    </>
+  )
+}
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Header />
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    
+    </>
+  )
+}
+
+export default App
